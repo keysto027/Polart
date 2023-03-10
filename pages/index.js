01bf476f1765547809a0f941b2494dcf31da0422
xX Keysto027 Xx <keysto395@gmail.com>
2023-03-10 09:10:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/POLART.ico" />
       </Head>
 
       <main>
