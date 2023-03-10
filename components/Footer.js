fab7f187337d8b73d9fd41ad75abbb96f7abd94a
xX Keysto027 Xx <keysto395@gmail.com>
2023-03-10 12:44:43 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/POLART.png" alt="Polart Logo" className={styles.logo} />
       </footer>
     </>
   )
