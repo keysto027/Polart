87315138de9cb5b4d0dd6ab08e0c45af8f9b80b8
xX Keysto027 Xx <keysto395@gmail.com>
2023-03-11 00:21:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <img src="/POLARTb.jpg" alt="carga" className="carga" />
         <Header title="WELCOME " />
         <p className= "tania">
-        NUEVO MUNDO hice un cambio
+        NUEVO MUNDO 
         </p>
         <p>
               Esto es Polart Estudio<code>pages/Mantenimiento</code>
