5c4208a205578397d2aade09b8cbfd9d0446a380
xX Keysto027 Xx <keysto395@gmail.com>
2023-06-21 21:34:59 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,65 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/netflix.ico" />
+      </Head>
+      <main>
+        <Header title="NETFLIX" />
+        <p className="tagline">
+          ¡Disfruta de miles de películas y series en streaming!
+        </p>
+        <button className="cta-button">Comenzar</button>
+      </main>
+
+      <Footer />
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          background-color: #000;
+          color: #fff;
+        }
+
+        .tagline {
+          font-size: 24px;
+          margin-top: 20px;
+          text-align: center;
+        }
+
+        .cta-button {
+          margin-top: 40px;
+          padding: 15px 30px;
+          font-size: 20px;
+          font-weight: bold;
+          background-color: #e50914;
+          color: #fff;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        .cta-button:hover {
+          background-color: #b2070e;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+
+
+/*import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
         <title>Polart Estudio!</title>
         <link rel="icon" href="/POLART.ico" />
       </Head>
@@ -27,3 +86,4 @@ export default function Home() {
     </div>
   )
 }
+*/
