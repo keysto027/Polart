8a04a461baf0be03ee9306b72da80c47bfcbd1f6
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-04 21:42:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
         </Link>
       </nav>
         <p className="tagline">
-          ¡EN MANTENIMIENTO POR no FAVOR ESPERE!
+          ¡EN MANTENIMIENTO POR nooFAVOR ESPERE!
         </p>
         <button className="cta-button">Comenzar</button>
       </main>
