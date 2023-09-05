10297a2f7845524303fd05b4e66018af38394642
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-05 15:07:23 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+/*import Head from 'next/head'
 import Link from 'next/link'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -44,7 +44,7 @@ export default function Home() {
       }
 
       .nav-link {
-        color: #000000; /* Color del texto de los enlaces de navegación */
+        color: #000000; /* Color del texto de los enlaces de navegación 
         text-decoration: none;
         margin-right: 20px;
         font-size: 15px;
@@ -79,7 +79,7 @@ export default function Home() {
           padding: 15px 30px;
           font-size: 20px;
           font-weight: bold;
-          background-color: rgb(255, 187, 0); /* Cambia el color aquí */
+          background-color: rgb(255, 187, 0); /* Cambia el color aquí 
           color: #fff;
           border: none;
           border-radius: 4px;
@@ -96,3 +96,100 @@ export default function Home() {
 
 
 /////////////////////////////////
+*/
+import Head from 'next/head'
+import Link from 'next/link'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>POLART ESTUDIOS</title>
+        <link rel="icon" href="/POLART.ico" />
+      </Head>
+      <main>
+        <Header title="POLART" style={{ float: 'left', marginRight: '20px' }} />
+        <button className="cta-button">Comenzar</button>
+        <nav className="nav">
+          <Link href="/">
+            <a className="nav-link">Inicio</a>
+          </Link>
+          <Link href="/nosotros">
+            <a className="nav-link">Nosotros</a>
+          </Link>
+          <Link href="/sugerencias">
+            <a className="nav-link">Sugerencias</a>
+          </Link>
+          <Link href="/empleo">
+            <a className="nav-link">Empleo</a>
+          </Link>
+          <Link href="/servicios">
+            <a className="nav-link">Servicios</a>
+          </Link>
+        </nav>
+        <p className="tagline">
+          ¡EN MANTENIMIENTO POR FAVOR ESPERE!
+        </p>
+      </main>
+
+      <Footer />
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          background-color: #000;
+          color: #fff;
+        }
+
+        .tagline {
+          font-size: 24px;
+          margin-top: 20px;
+          text-align: center;
+        }
+
+        .cta-button {
+          margin-top: 40px;
+          padding: 15px 30px;
+          font-size: 20px;
+          font-weight: bold;
+          background-color: rgb(255, 187, 0);
+          color: #fff;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        .cta-button:hover {
+          background-color: rgb(69, 104, 196);
+        }
+
+        .nav {
+          display: flex;
+          justify-content: flex-end;
+          align-items: flex-start;
+        }
+
+        .nav-link {
+          color: #000000;
+          text-decoration: none;
+          margin-right: 20px;
+          font-size: 15px;
+          font-weight: bold;
+          background-color: rgb(255, 187, 0);
+          border: none;
+          border-radius: 25px;
+          cursor: pointer;
+        }
+
+        .nav-link:hover {
+          background-color: rgb(69, 104, 196);
+        }
+      `}</style>
+    </div>
+  )
+}
