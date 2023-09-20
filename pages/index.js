ab04722df921cd7447d9b39fd95b5638fad4d0bd
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-19 21:49:46 -0400

    Update:Index.js

--- pages/index.js
+++ pages/index.js
@@ -140,8 +140,6 @@ export default function Home() {
         </div>
         <div>
           
-          
-          <button className="cta-button">Comenzar</button>
         </div>
       </main>
 
