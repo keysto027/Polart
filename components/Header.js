0f8db288c3efcf971905a21e701be0ffa21e6fbc
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-20 00:50:58 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,26 @@
+import Link from 'next/link'
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <div>
+      <h1 className="title">{title}</h1>
+      <nav className="nav">
+        <Link href="/">
+          <a className="nav-link">Inicio</a>
+        </Link>
+        <Link href="/nosotros">
+          <a className="nav-link">Nosotros</a>
+        </Link>
+        <Link href="/Novedades">
+          <a className="nav-link">Novedades</a>
+        </Link>
+        <Link href="/empleo">
+          <a className="nav-link">Empleo</a>
+        </Link>
+        <Link href="/servicios">
+          <a className="nav-link">Servicios</a>
+        </Link>
+      </nav>
+    </div>
+  );
 }
