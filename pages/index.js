4eb5edf8baaf74d351e63e3082acf2e1678f271c
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-20 01:49:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+
 import Head from 'next/head'
-import Link from 'next/link'
+
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -11,25 +12,7 @@ export default function Home() {
         <link rel="icon" href="/Costa.ico" />
       </Head>
       <main>
-      
-        <Header title="Costa norte" />
-        <nav className="nav">
-        <Link href="/">
-          <a className="nav-link">Inicio</a>
-        </Link>
-        <Link href="/nosotros">
-          <a className="nav-link">Nosotros</a>
-        </Link>
-        <Link href="/Novedades">
-          <a className="nav-link">Novedades</a>
-        </Link>
-        <Link href="/empleo">
-          <a className="nav-link">Empleo</a>
-        </Link>
-        <Link href="/servicios">
-          <a className="nav-link">Servicios</a>
-        </Link>
-      </nav>
+       <Header logoSrc="/Logo.png" />
         
         <p className="tagline">
           ¡EN MANTENIMIENTO POR FAVOR ESPERE a Cailito!
