4aca0357ef37deeb6da2344a98cda76f58d1e97d
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-20 00:32:54 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -97,45 +97,44 @@ export default function Home() {
 
 /////////////////////////////////
 */
-import Head from 'next/head';
-import Link from 'next/link';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Head from 'next/head'
+import Link from 'next/link'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>COSTA </title>
+        <title>POLART ESTUDIOS</title>
         <link rel="icon" href="/Costa.ico" />
       </Head>
-
       <main>
-        <div className="fondo">
-          <div className="header-and-button">
-          <Header title="costa norte" />
-
-            <nav className="nav">
-              <Link href="/">
-                <a className="nav-link">Inicio</a>
-              </Link>
-              <Link href="/Nosotros">
-                <a className="nav-link">Nosotros</a>
-              </Link>
-              <Link href="/Novedades">
-                <a className="nav-link">Novedadess</a>
-              </Link>
-              <Link href="/empleo">
-                <a className="nav-link">Empleo</a>
-              </Link>
-              <Link href="/servicios">
-                <a className="nav-link">Servicios</a>
-              </Link>
-            </nav>
-          </div>
-        </div>
-        <div></div>
+      <button className="cta-button">Comenzar</button>
+        <Header title="Costa norte" />
+        <nav className="nav">
+        <Link href="/">
+          <a className="nav-link">Inicio</a>
+        </Link>
+        <Link href="/nosotros">
+          <a className="nav-link">Nosotros</a>
+        </Link>
+        <Link href="/sugerencias">
+          <a className="nav-link">Sugerencias</a>
+        </Link>
+        <Link href="/empleo">
+          <a className="nav-link">Empleo</a>
+        </Link>
+        <Link href="/servicios">
+          <a className="nav-link">Servicios</a>
+        </Link>
+      </nav>
+        <p className="tagline">
+          ¡EN MANTENIMIENTO POR FAVOR ESPERE!
+        </p>
+        
       </main>
+
       <Footer />
     </div>
   );
