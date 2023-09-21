71b36902e849767c67017ebaeff3a008ff575971
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-21 12:10:44 -0400

    update novedades.css

--- pages/Nosotros.js
+++ pages/Nosotros.js
@@ -11,7 +11,7 @@ export default function Nosotros() {
         <title>Nosotros - COSTA NORTE</title>
         <link rel="icon" href="/Costa.ico" />
       </Head>
-
+      
       <main>
       <Header logoSrc="/Logo.png" />
         <div className="fondo">
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '@styles/globals.css'
+import '@styles/Nosotros.css'
+import '@styles/Novedades.css'
+
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
