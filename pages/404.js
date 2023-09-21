faa3f73aed4b249f4e34197a5fb64f40bfc954c1
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-21 02:00:27 -0300

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+// Importa los módulos necesarios
+import Head from 'next/head';
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import { Main } from 'next/document';
+
+// Componente de la página Novedades
+export default function _404() {
+  return (
+<main>
+
+<Header logoSrc="/Logo.png" />
+
+</main>
+    
+  )}
