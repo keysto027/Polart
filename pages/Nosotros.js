33dc34ec3bc11fd3f85618820baf13006de1deaa
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-21 20:11:28 -0400

    update Component reac

--- pages/Nosotros.js
+++ pages/Nosotros.js
@@ -2,6 +2,7 @@
 import Head from 'next/head';
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import react from "react";
 
 // Componente de la página Nosotros
 export default function Nosotros() {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 import Head from 'next/head'
-
+import react from "react";
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
