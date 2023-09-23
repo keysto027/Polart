200cda9a5dbd52b8ccf75face7eb96afae7e6bdf
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-22 21:27:25 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -62,12 +62,6 @@ export default function Header({ logoSrc  }) {
               
             </ul>
         </header>
-        <div className={styles.content}>
-          <h2>Costa Norte</h2>
-          <p>Brindando atencion medica desde 1996</p>
-          <a href='#'>Consulta</a>
-          <a href='#'>Resultados</a>
-        </div>
       </div>
     </section>
   
