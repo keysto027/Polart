d7d7d196527b7f17bdaa66799b0143de705d040f
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-23 14:07:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,32 +12,30 @@ export default function Home() {
         <link rel="icon" href="/Costa.ico" />
       </Head>
       <main>
-       <div className="container1">
 
-        <div class= "imgBx"> 
-        <img src="/recepcion.gif"></img>
+      <div className="container1">
+          <div class= "imgBx"> 
+            <img src="/recepcion.gif"></img>
         </div>
+      </div>
 
-       </div>
        <div className="container2">
-       
          <div className="content2">
-      
            <img  className="img1"  src="/reloj.gif"></img>
            <h2>SERVICIO 24/7</h2> 
            <h3>Horario</h3>
         </div>
-
         <div className="content3">
           <img className="llamada" src="/llamada.gif"></img>
           <h2>Telefono</h2>
           <h3>+1 809-584-3939</h3>
         </div>
 
-        <div className="content4">
+        <div className="content4" href="/Nosotros">
           <img className="ubicacion" src="/ubicacion.gif"></img>
-          <h2>Telefono</h2>
-          <h3>+1 809-584-3939</h3>
+          <h2>Ubicacion</h2>
+          <h3>Ave. María Trinidad Sánchez No.1</h3>
+          <h4>Nagua - Rep. Dom.</h4>
         </div>
 
        </div>
@@ -46,7 +44,13 @@ export default function Home() {
        <div className="content">
           <h2>Costa Norte</h2>
           <p>Brindando atencion medica desde 1996</p>
-       </div>
+          <div className="buttoms">
+            <a className="Consultas" href="Consultas">Consultas</a>
+            <a className="Resultados"href="Resultados">Resultados</a>  
+          </div>
+        </div>
+        
+
        
 
       
