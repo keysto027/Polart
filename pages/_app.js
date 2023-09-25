0b5fe53397c91ace265638000f4f0e9a81c673af
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-24 23:18:11 -0400

    asd

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '@styles/globals.css'
-
 import '@styles/Novedades.css'
-
-
 import '@components/Header.module.css'
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
