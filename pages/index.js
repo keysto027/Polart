e94774da3120e84a76670d529b42a60911070a18
Tewck <106410060+Tewck@users.noreply.github.com>
2023-09-27 12:59:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,19 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import Swiper from 'swiper/bundle';
 
+
 export default function Home() {
  
   useEffect(() => {
     var swiper = new Swiper(".mySwiper", {
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      autoplay:{
-        delay:2000,
-        disableOnInteraction:false,
-
-      },
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 15,
-        stretch: 0,
-        depth: 300,
-        modifier: 1,
-        slideShadows: true,
+      autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
       },
       loop: true,
     });
-  }, []); 
-
+  }, []);
+  
 
   return (
     <section className="section">
@@ -41,87 +30,14 @@ export default function Home() {
             href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
           <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
         </Head>
+
         <main>
+
           <div className="container1">
             <div className="imgBx">
               <img src="/recepcion.gif" alt="Recepción" />
             </div>
           </div>
-        
-         <div className="container3">
-
-                <div class="swiper mySwiper">
-
-                  <div class="swiper-wrapper">
-
-                <div class="swiper-slide">
-                 <img src="cirugia.jpg" />
-                  </div>
-
-                  <div class="swiper-slide">
-                   <img src="consulta-medica.jpg" />
-                         </div>
-
-                      <div class="swiper-slide">
-                        <img src="imagenes-medicas.jpg" />
-                       </div>
-
-                   <div class="swiper-slide">
-                 <img src="servicios-laboratorio.jpg" />
-                      </div>
-
-                     <div class="swiper-slide">
-                      <img src="UCI.jpg" />
-                </div>
-
-            <div class="swiper-slide">
-             <img src="UCIN.jpg" />
-           </div>
-
-            </div>
-
-              <div class="swiper-pagination"></div>
-                </div>
-
-
-
-         </div>
-    
-          
-
-          <div className="container2">
-            <div className="content2">
-              <img className="img1" src="/reloj.gif" alt="Reloj" />
-              <h2>SERVICIO 24/7</h2>
-              <h3>Horario</h3>
-            </div>
-
-            
-          
-
-
-           
-
-
-            <div className="content3">
-              <img className="llamada" src="/llamada.gif" alt="Llamada" />
-              <h2>Telefono</h2>
-              <h3>+1 809-584-3939</h3>
-            </div>
-
-            <div className="content4" href="/Nosotros">
-              <img
-                className="ubicacion"
-                src="/ubicacion.gif"
-                alt="Ubicación"
-              />
-              <h2>Ubicacion</h2>
-              <h3>Ave. María Trinidad Sánchez No.1</h3>
-              <h4>Nagua - Rep. Dom.</h4>
-            </div>
-
-            </div>
-          
 
           <div className="content">
             <h2>Costa Norte</h2>
@@ -135,6 +51,38 @@ export default function Home() {
               </a>
             </div>
           </div>
+      
+          <div className="container2">
+            <div className="content2">
+              <img className="img1" src="/reloj.gif" alt="Reloj" />
+              <h2>SERVICIO 24/7</h2>
+              <h3>Horario</h3> 
+            </div>
+            <div className="content3">
+              <img className="llamada" src="/llamada.gif" alt="Llamada" />
+              <h2>Telefono</h2>
+              <h3>+1 809-584-3939</h3>
+            </div>
+            <div className="content4" href="/Nosotros">
+              <img className="ubicacion" src="/ubicacion.gif" alt="Ubicación"/>
+              <h2>Ubicacion</h2>
+              <h3>Ave. María Trinidad Sánchez No.1</h3>
+              <h4>Nagua - Rep. Dom.</h4>
+            </div>
+          </div>
+        
+         <div className="container3">
+            <div class="swiper mySwiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide"> <img src="cirugia1.jpg" /> </div>
+                <div class="swiper-slide"> <img src="consulta-medica1.jpg" /> </div>
+              
+                </div>
+              </div>
+            </div>
+
+            
+      
 
           <Header logoSrc="/Logo.png" />
         </main>
