469133a12efb96384822e9c76927f42cca38eb6f
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-29 23:51:29 -0300

    Update Nosotros.js

--- pages/Nosotros.js
+++ pages/Nosotros.js
@@ -27,14 +27,42 @@ export default function Nosotros() {
 
       <Header logoSrc="/Logo.png" />
         
-          <section className={styles.nosotros}>
-          
-          </section>
-        
+          <div className={styles.Historia}>
+            
+            <h2>Nuestros Inicios</h2>
+            <p className={styles.p1}>
+            El 14 de enero de 1996 comenzó a laborar el Centro de Especialidades Médicas Costa Norte, 
+            ubicado para ese entonces en la 
+            Calle Miguel José No. 21 de Nagua, Provincia María Trinidad Sánchez, República Dominicana.
+            </p>
+            <p className={styles.p2}>
+            Los médicos unidos en ese proyecto, ya eran médicos conocidos por sus condiciones de especialistas, 
+            aferrados a la ética profesional, con 5 o más años laborando en la región. La constancia de los servicios
+             médicos prestados, hicieron que el Centro se convirtiera en el lugar de referencia de los demás centros de 
+             salud de la región, lo cual inmediatamente nos convirtió en un centro médico de atención regional; quedando 
+             nuestra institución con la responsabilidad de recibir y asistir los problemas de salud desde Gaspar Hernández 
+             hasta las Galeras de Samaná y la zona baja del Yuna, incluyendo
+             Villa Riva y Arenoso; aun perteneciendo éstas, geográficamente a San Francisco de Macorís.
+            </p>
+            <p className={styles.p3}>
+            Por estas razones, los principales hoteles turísticos y empresas de la región le
+             solicitaron a sus compañías aseguradoras de salud, afiliar nuestro Centro Médico a sus 
+             servicios, por considerar que en nuestra institución cuenta con
+             el servicio más óptimo de la región, para la atención de empleados y huéspedes.
+            </p>
+            <p className={styles.p4}>
+            El crecimiento alcanzado por la institución trajo consigo un problema: espacio físico.
+             Es aquí donde entendimos que la construcción de un moderno edificio, era una necesidad imperiosa.
+              Siendo así las cosas, construimos una moderna infraestructura, ubicada en una vía de fácil acceso, 
+              tanto para los que vienen desde Gaspar Hernández, Río San Juan y Cabrera, como los que vienen de Samaná, 
+              Las Terrenas y Sánchez;
+             de igual forma los que proceden de Villa Riva, Arenoso y todo el extenso Yuna.
+            </p>
 
-        <div className={styles.imgbx1}>
-               <img src="/seccion2.jpg"></img>
-        </div>
+
+          </div>
+
+        
       </main>
       </div>
       <Footer />
