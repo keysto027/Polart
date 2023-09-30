6f1cceb6bb835a47cc3243cbd5cb7fa938d3b908
xX Keysto027 Xx <keysto395@gmail.com>
2023-09-30 02:26:14 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,51 +3,9 @@ import styles from './Header.module.css'
 
 export default function Header({ logoSrc  }) {
   return (
-    // <div>
-    //   <header className={styles.header}>
-    //     <div>
+  
 
-    //     </div>
-    //   </header>
-      
-      
-    //   <nav className={styles.nav}>
-    //   <img src="/Logo.png" alt="Costa Norte" className={styles.logo} />
-    //     <Link href="/">
-    //       <a className={styles.navlink}>Inicio</a>
-    //     </Link>
-    //     <Link href="/Nosotros">
-    //       <a className={styles.navlink}>Nosotros</a>
-    //     </Link>
-    //     <Link href="/Novedades">
-    //       <a className={styles.navlink}>Novedades</a>
-    //     </Link>
-
-      
-    //     <Link href="/servicios">
-    //       <a className={styles.navlink}>Servicios</a>
-         
-    //     </Link>
-      
-    //     <Link href="/empleo">
-    //       <a className={styles.navlink}>Empleo</a>
-    //     </Link>
-    //     <Link href="/servicios">
-    //       <a className={styles.navlink}>Servicios</a>
-    //     </Link>
-    //     <Link href="/Sucursales">
-    //       <a className={styles.navlink}>Sucursales</a>
-    //     </Link>
-    //     <Link href="/Contacto">
-    //       <a className={styles.navlink}>Contacto</a>
-    //     </Link>
-    //     <Link href="/Resultados">
-    //       <a className={styles.navlink}>Resultados</a>
-    //     </Link>
-    //   </nav>
-    // </div>
-
-    
+  
       <div class={styles.container}>
         <header className={styles.header}>
         <img src="/Logo.png" alt="Costa Norte" className={styles.logo} />
