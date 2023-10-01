e61d9c3ee611c7e8443791ca59be7b151fb3db29
Tewck <106410060+Tewck@users.noreply.github.com>
2023-10-01 15:32:37 -0400

    Update Servicios.js

--- pages/Servicios.js
+++ pages/Servicios.js
@@ -90,6 +90,56 @@ export default function Servicios()
                         <p>La otorrinolaringología a veces abreviada como ORL es una rama de la medicina que se encarga del diagnóstico y tratamiento de las enfermedades y problemas relacionados con la cabeza y el cuello. En particular, se enfoca en el oído, la nariz, la garganta y la parte superior de la tráquea. Los médicos que se especializan en ORL son los otorrinolaringólogos.</p>
                     </div>
 
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='intensivist.png' alt='itensivist'/> </div>
+                        <h2>Itensivista</h2>
+                        <p>Un intensivista es una persona que ha especializado su carrera en el cuidado de pacientes críticos en una UCI, o unidad de cuidados intensivos. Este tipo de médicos suelen trabajar con otros especialistas como anestesiólogos y cirujanos, y son responsables de la atención de pacientes que están bajo monitoreo continuo y que están en un estado de salud delicado.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='pneumology.png' alt='Neumologia'/> </div>
+                        <h2>Neumologia</h2>
+                        <p>La neumología es una rama de la medicina dedicada a los pulmones y el aparato respiratorio. Dentro de esta rama, los neumólogos se enfocan en la prevención, diagnóstico y tratamiento de enfermedades pulmonares tales como asma, neumonía, tuberculosis, pulmonía, fibrosis quística y problemas respiratorios crónicos.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='Endocrinology.png' alt='Endocrinologia'/> </div>
+                        <h2>Endocrinologia</h2>
+                        <p>La endocrinología es la rama de la medicina que estudia los hormonas y el sistema endocrino. Un endocrinólogo es un médico que se especializa en tratar trastornos y patologías relacionadas con el sistema endocrino, como el metabolismo, la diabetes, la menopausia y los trastornos de la tiroides.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='hematology.png' alt='hematologia'/> </div>
+                        <h2>Hematologia</h2>
+                        <p>La hematología es otra rama de la medicina. Los hematólogos estudian el sistema circulatorio y el sistema de producción de células sanguíneas, y se enfocan en las enfermedades relacionadas con estos sistemas, como anemia, enfermedades de la sangre, leucemia y más.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='gastroenterology.png' alt='Gastroenterologia'/> </div>
+                        <h2>Gastroenterologia</h2>
+                        <p>La gastroenterología es una rama de la medicina que se enfoca en el sistema digestivo. Los gastroenterólogos son médicos que se especializan en el tratamiento de enfermedades relacionadas con el tracto digestivo, como los problemas de la estructura y función de la boca, estómago, intestino, colon y ano.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='Dermatology.png' alt='Dermatology'/> </div>
+                        <h2>Dermatologia</h2>
+                        <p>La dermatología es la rama de la medicina que se enfoca en el estudio y tratamiento de las enfermedades de la piel. Los dermatólogos son médicos que se enfocan en los trastornos y problemas de la piel, como el acné, eczema, psoriasis y cáncer de piel.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='psychology.png' alt='Psicologia'/> </div>
+                        <h2>Psicologia</h2>
+                        <p>La psicología es la ciencia que se enfoca en el comportamiento humano, el desarrollo de la persona, y la mente y emociones. Los psicólogos son profesionales que ayudan a las personas a entender y tratar problemas mentales, emocionales y de comportamiento.</p>
+                    </div>
+
+                    <div className={styles.cards}>
+                        <div className={styles.cover_cards}> <img src='sonography.png' alt='sonogragia'/> </div>
+                        <h2>Sonografia</h2>
+                        <p>La sonografía, también conocida como ultrasonido o ecografía, es una técnica médica que usa ondas de sonido para crear imágenes del interior del cuerpo. Es un método de diagnóstico seguro y sin riesgos, ya que no usa radiación. Las imágenes sonográficas son usadas para ayudar a identificar y evaluar diversas condiciones médicas.</p>
+                    </div>
+                    
+                    
+
                     
 
                 </div>
