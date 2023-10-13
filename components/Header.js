f779d002ad28f7d2e54c7cc5a0bbe351868b1c04
xX Keysto027 Xx <keysto395@gmail.com>
2023-10-13 17:22:56 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,35 @@
-import Link from 'next/link'
-import styles from './Header.module.css'
-//import styles from './HeaderMobile.module.css'
-import { useState } from "react";
+import React, { useState } from "react";
+import styles from './Header.module.css';
 
+export default function Header({ logoSrc }) {
+  const [isChecked, setIsChecked] = useState(false); // Define isChecked y su función de actualización
 
-export default function Header({ logoSrc  }) {
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked); // Actualiza el valor de isChecked al cambiar el checkbox
+  };
+
   return (
-  
-
-  
-      <div class={styles.container}>
-        <header className={styles.header}>
-
+    <div className={styles.container}>
+      <header className={styles.header}>
         <img src="/Logo.png" alt="Costa Norte" className={styles.logo} />
-
-        <input> type="checkbox"
-           id={styles.check}
-            checked={isChecked}
-                 onChange={handleCheckboxChange}
-                      </input>
-          
-
-              <label htmlFor={styles.check} className={styles.icons}>
-                <i className={`bx {isChecked ? styles.closeicon : styles.menuicon}`}></i>
-            </label>
-            <i class='bx bx-menu' id={styles.menuicon}></i>
-            <i class='bx bx-x' id={styles.closeicon}></i> 
-        
-            <ul>
-              <li><a href="/">Inicio</a></li>
-              <li><a href="/Nosotros">Nosotros</a></li>
-              <li><a href="/Servicios">Servicios</a></li>
-              <li><a href="/Novedades">Novedades</a></li>
-              <li><a href="/Empleo">Empleo</a></li>
-              <li><a href="/Contacto">Contacto</a></li>
-            </ul>            
-        </header>
-      </div>
-
-      
-   
-  
-    
-
+        <input
+          type="checkbox"
+          id={styles.check}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor={styles.check} className={styles.icons}>
+          <i className={`bx ${isChecked ? styles.closeicon : styles.menuicon}`}></i>
+        </label>
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/Nosotros">Nosotros</a></li>
+          <li><a href="/Servicios">Servicios</a></li>
+          <li><a href="/Novedades">Novedades</a></li>
+          <li><a href="/Empleo">Empleo</a></li>
+          <li><a href="/Contacto">Contacto</a></li>
+        </ul>
+      </header>
+    </div>
   );
 }
-
-
-
-
-
-
