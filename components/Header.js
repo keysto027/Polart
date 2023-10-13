c361ab0c9a9f70e28ab07ae685cab97587392bcc
xX Keysto027 Xx <keysto395@gmail.com>
2023-10-13 17:30:58 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,44 @@
-import React, { useState } from "react";
-import styles from './Header.module.css';
+import Link from 'next/link'
+import styles from './Header.module.css'
+import Mobilestyles from './HeaderMobile.module.css'
+import { useState } from "react";
 
-export default function Header({ logoSrc }) {
-  const [isChecked, setIsChecked] = useState(false); // Define isChecked y su función de actualización
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked); // Actualiza el valor de isChecked al cambiar el checkbox
-  };
-
+export default function Header({ logoSrc  }) {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
+  
+
+  
+      <div class={styles.container}>
+        <header className={styles.header}>
+
         <img src="/Logo.png" alt="Costa Norte" className={styles.logo} />
-        <input>
-          type="checkbox"
-          id={styles.check}
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-        </input>
-        <label htmlFor={styles.check} className={styles.icons}>
-          <i className={`bx ${isChecked ? styles.closeicon : styles.menuicon}`}></i>
+        <input type="checkbox" id="check"></input>
+        <label for="" className={styles.icons}>
+            <i class='bx bx-menu' id="menu-icon"></i>
+            <i class='bx bx-x' id="close-icon"></i> 
         </label>
-        <ul>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="/Nosotros">Nosotros</a></li>
-          <li><a href="/Servicios">Servicios</a></li>
-          <li><a href="/Novedades">Novedades</a></li>
-          <li><a href="/Empleo">Empleo</a></li>
-          <li><a href="/Contacto">Contacto</a></li>
-        </ul>
-      </header>
-    </div>
+            <ul>
+              <li><a href="/">Inicio</a></li>
+              <li><a href="/Nosotros">Nosotros</a></li>
+              <li><a href="/Servicios">Servicios</a></li>
+              <li><a href="/Novedades">Novedades</a></li>
+              <li><a href="/Empleo">Empleo</a></li>
+              <li><a href="/Contacto">Contacto</a></li>
+            </ul>            
+        </header>
+      </div>
+
+      
+   
+  
+    
+
   );
 }
+
+
+
+
+
+
